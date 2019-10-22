712b36bf68396db58a4ed20e6edacb492b78fad5
Alejandro Brugarolas <alejandro_brugarolas@hotmail.com>
2019-10-22 23:24:53 +0200

    Last exercise

--- Seminario0/main.c
+++ Seminario0/main.c
@@ -12,6 +12,7 @@ void showMenu();
 void sumArrays();
 void jacobiSolver();
 void doJacobi(float *array1, float *array2, int arraySize);
+void sumFloatArray();
 
 /**
  * Shows all the options and calls the appropriate function depending of the chosen option
@@ -20,13 +21,14 @@ void showMenu() {
 
     int option = 0;
 
-    while (option != 3) {
+    while (option != 4) {
         //Todo seleccionar algoritmo y mostrar menú distinto o más opciones por cada algoritmo
         printf("\n############### MENU SEMINARIO 0 ###############\n"
                "Indique qué acción desea realizar\n"
                "\t1. Suma de arrays\n"
                "\t2. Método de Jacobi\n"
-               "\t3. Salir\n");
+               "\t3. Suma Array de Float\n"
+               "\t4. Salir\n");
         printf("> ");
 
         scanf("%d", &option);
@@ -39,6 +41,9 @@ void showMenu() {
                 jacobiSolver();
                 break;
             case 3:
+                sumFloatArray();
+                break;
+            case 4:
                 printf("Saliendo...");
                 break;
             default:
@@ -50,51 +55,53 @@ void showMenu() {
 
 /**
  * Asks the user for a size then sums two 2D arrays of sizeXsize ints
- * This can be optimized by removing some loops or even using a 1D array with row major order to simulate a 2D one
+ * This can be optimized by removing some loops
  */
 void sumArrays(){
-    int size = 0;
-    int **array1, **array2, **arrayResult;
+    int size = 0, showResult = 0;
+    int position = 0; //Aux variable to calculate a position in the 2D array
+    int *array1, *array2, *arrayResult;
+    printf("¿Desea mostrar el resultado de la suma por pantalla?\n\t1. Sí\n\t2. No\n> ");
+    scanf("%d", &showResult);
+    fflush(stdin);
+
     printf("Introduzca el tamaño de los arrays\n> ");
     scanf("%d", &size);
     fflush(stdin);
     printf("Rellenando los arrays bidimensionales de %d X %d con valores aleatorios...\n", size, size);
 
-    //Allocates the memory and fill the arrays with random values
-    array1 = (int**) malloc(sizeof(int) * size);
-    array2 = (int**) malloc(sizeof(int) * size);
+    //Allocates the memory
+    array1 = (int*) malloc(sizeof(int) * size * size);
+    array2 = (int*) malloc(sizeof(int) * size * size);
     for (int i = 0; i < size; ++i) {
-        array1[i] = (int*) malloc(sizeof(int) * size);
-        array2[i] = (int*) malloc(sizeof(int) * size);
         for (int j = 0; j < size; ++j) {
-            array1[i][j] = rand();
-            array2[i][j] = rand();
+            position = i * size + j;
+            array1[position] = rand();
+            array2[position] = rand();
         }
     }
 
     printf("Sumando los arrays...\n", size, size);
 
-    arrayResult = (int**) malloc(sizeof(int) * size);
+    arrayResult = (int*) malloc(sizeof(int) * size * size);
     for (int i = 0; i < size; ++i) {
-        arrayResult[i] = (int*) malloc(sizeof(int) * size);
         for (int j = 0; j < size; ++j) {
-            arrayResult[i][j] = array1[i][j] + array2[i][j];
+            position = i * size + j;
+            arrayResult[position] = array1[position] + array2[position];
         }
     }
 
-    printf("Suma realizada correctamente, se muestran los resultados a continuación\n");
-
-    for (int i = 0; i < size; ++i) {
-        for (int j = 0; j < size; ++j) {
-            printf("Resultado[%d][%d] = %d\n", i, j, arrayResult[i][j]);
+    if(showResult == 1){
+        printf("Suma realizada correctamente, se muestran los resultados a continuación\n");
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j < size; ++j) {
+                position = i * size + j;
+                printf("Resultado[%d][%d] = %d\n", i, j, arrayResult[position]);
+            }
         }
     }
 
-    for (int i = 0; i < size; ++i) {
-        free(array1[i]);
-        free(array2[i]);
-        free(arrayResult[i]);
-    }
+    //Releases the memory previously allocated
     free(array1);
     free(array2);
     free(arrayResult);
@@ -129,6 +136,7 @@ void jacobiSolver(){
         }
     }
 
+    printf("Aplicando el método...\n");
     //The Jacobi's method itself
     float *aux;
     for (int k = 0; k < iterations; ++k) {
@@ -137,6 +145,8 @@ void jacobiSolver(){
         array1 = array2;
         array2 = aux;
     }
+
+    printf("Método aplicado correctamente.\n");
 
     //Releases the memory previously allocated
     free(array1);
@@ -157,6 +167,41 @@ void doJacobi(float *array1, float *array2, int arraySize){
             array2[position] = 0.2f * (array1[position] + array1[position - 1] + array1[position + 1] + array1[position + arraySize] + array1[position - arraySize]);
         }
     }
+}
+
+
+
+/**
+ * Asks the user for a size then sums two 1D array of random floats
+ */
+void sumFloatArray(){
+    int size = 0;
+    float result = 0.0f;
+    float *array;
+
+    printf("Introduzca el tamaño del array\n> ");
+    scanf("%d", &size);
+    fflush(stdin);
+    printf("Rellenando el array...\n");
+
+    //Allocates the memory
+    array = (float *) malloc(sizeof(float) * size);
+
+    for (int i = 0; i < size; ++i) {
+        array[i] = 1.0f;
+    }
+
+    printf("Sumando los %d valores del array...\n", size);
+
+
+    for (int i = 0; i < size; ++i) {
+        result += array[i];
+    }
+
+    printf("Suma realizada correctamente, el resultado es: %f\n", result);
+
+    //Releases the memory previously allocated
+    free(array);
 }
 
 int main() {
