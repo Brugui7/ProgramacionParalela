9741d47a6988f095de318b7c27cf5670bd192c68
Alejandro Brugarolas <alejandro_brugarolas@hotmail.com>
2019-11-23 14:05:08 +0100

    exercise 6

--- OpenMPExercises/firstPart/SecondPart.c
+++ OpenMPExercises/firstPart/SecondPart.c
@@ -9,7 +9,7 @@
  * Asks for the number of threads to be used in the function
  * @return int
  */
-int getThreadNumber(){
+int getThreadNumber() {
     int threadNumber;
     printf("Introduzca el número de hilos a ejecutar\n >");
     scanf("%d", &threadNumber);
@@ -17,23 +17,23 @@ int getThreadNumber(){
     return threadNumber;
 }
 
-void exercise1Function1(){
+void exercise1Function1() {
     int a = 0;
     int b = 3;
-    for(int i = 0; i < 1000000000; i++){
-        a += i*b;
+    for (int i = 0; i < 1000000000; i++) {
+        a += i * b;
     }
 }
 
-void exercise1Function2(){
+void exercise1Function2() {
     int a = 0;
     int b = 5;
-    for(int i = 0; i < 1000000000; i++){
-        a += i*b;
+    for (int i = 0; i < 1000000000; i++) {
+        a += i * b;
     }
 }
 
-void exercise1(){
+void exercise1() {
     //For time measuring
     struct timeval start, end;
     double timeInvested;
@@ -55,13 +55,13 @@ void exercise1(){
     omp_set_num_threads(threads);
 
     gettimeofday(&start, NULL);
-    #pragma omp parallel sections
+#pragma omp parallel sections
     {
-        #pragma omp section
+#pragma omp section
         { exercise1Function1(); }
 
-        #pragma omp section
-        {exercise1Function2(); }
+#pragma omp section
+        { exercise1Function2(); }
 
     };
 
@@ -71,7 +71,7 @@ void exercise1(){
     printf("-----------------\nTiempo invertido: %f\n-----------------\n", timeInvested);
 }
 
-void exercise2(){
+void exercise2() {
 
     //For time measuring
     struct timeval start, end;
@@ -79,16 +79,16 @@ void exercise2(){
     int size = 1073741824;
 
     double *x1, *x2, *x3;
-    double y1 = 0.0f, y2 = 0.0f , y3 = 0.0f;
-    x1 = (double*) malloc(sizeof(double) * size);
-    x2 = (double*) malloc(sizeof(double) * size);
-    x3 = (double*) malloc(sizeof(double) * size);
+    double y1 = 0.0f, y2 = 0.0f, y3 = 0.0f;
+    x1 = (double *) malloc(sizeof(double) * size);
+    x2 = (double *) malloc(sizeof(double) * size);
+    x3 = (double *) malloc(sizeof(double) * size);
 
     int j = 0;
 
     printf("Rellenando arrays de tamaño %d...\n", size);
 
-    for( int i = 0; i < size; i++){
+    for (int i = 0; i < size; i++) {
         x1[i] = 1.0f;
         x2[i] = 1.0f;
         x3[i] = 1.0f;
@@ -103,7 +103,7 @@ void exercise2(){
         omp_set_num_threads(i);
         printf("\nNúmero de hilos: %d", i);
 
-        #pragma omp parallel for schedule(static) reduction(+:y1)
+#pragma omp parallel for schedule(static) reduction(+:y1)
         for (j = 0; j < size; j++) {
             y1 += x1[j];
         }
@@ -122,7 +122,7 @@ void exercise2(){
 
         omp_set_num_threads(i);
         printf("\nNúmero de hilos: %d", i);
-        #pragma omp parallel for schedule(dynamic) reduction(+:y2)
+#pragma omp parallel for schedule(dynamic) reduction(+:y2)
         for (j = 0; j < size; j++) {
             y2 += x2[j];
         }
@@ -141,7 +141,7 @@ void exercise2(){
 
         omp_set_num_threads(i);
         printf("\nNúmero de hilos: %d", i);
-        #pragma omp parallel for schedule(guided)
+#pragma omp parallel for schedule(guided)
         for (j = 0; j < size; j++) {
             y3 += x3[j];
         }
@@ -158,31 +158,31 @@ void exercise2(){
     free(x3);
 }
 
-void exercise3(){
+void exercise3() {
 
     //For time measuring
     struct timeval start, end;
     double timeInvested;
 
     double *x1, *x2, *x3;
-    double y1 = 0.0f, y2 = 0.0f , y3 = 0.0f;
-    x1 = (double*) malloc(sizeof(double) * 3000);
-    x2 = (double*) malloc(sizeof(double) * 4000);
-    x3 = (double*) malloc(sizeof(double) * 5000);
+    double y1 = 0.0f, y2 = 0.0f, y3 = 0.0f;
+    x1 = (double *) malloc(sizeof(double) * 3000);
+    x2 = (double *) malloc(sizeof(double) * 4000);
+    x3 = (double *) malloc(sizeof(double) * 5000);
 
     int j = 0;
 
     printf("Rellenando arrays...\n");
 
-    for(int i = 0; i < 3000; i++){
+    for (int i = 0; i < 3000; i++) {
         x1[i] = 2.0f;
     }
 
-    for(int i = 0; i < 4000; i++){
+    for (int i = 0; i < 4000; i++) {
         x2[i] = 2.0f;
     }
 
-    for(int i = 0; i < 5000; i++){
+    for (int i = 0; i < 5000; i++) {
         x3[i] = 2.0f;
     }
 
@@ -200,7 +200,7 @@ void exercise3(){
         omp_set_num_threads(i);
         printf("\nNúmero de hilos: %d", i);
 
-        #pragma omp parallel for reduction(+:y1)
+#pragma omp parallel for reduction(+:y1)
         for (j = 0; j < 3000; j++) {
             y1 += x1[j] * x1[j];
         }
@@ -220,7 +220,7 @@ void exercise3(){
 
         omp_set_num_threads(i);
         printf("\nNúmero de hilos: %d", i);
-        #pragma omp parallel for reduction(+:y2)
+#pragma omp parallel for reduction(+:y2)
         for (j = 0; j < 4000; j++) {
             y2 += x2[j] * x2[j];
         }
@@ -240,7 +240,7 @@ void exercise3(){
 
         omp_set_num_threads(i);
         printf("\nNúmero de hilos: %d", i);
-        #pragma omp parallel for reduction(+:y3)
+#pragma omp parallel for reduction(+:y3)
         for (j = 0; j < 5000; j++) {
             y3 += x3[j] * x3[j];
         }
@@ -265,9 +265,9 @@ void exercise3(){
         omp_set_num_threads(i);
         printf("\nNúmero de hilos: %d", i);
 
-        #pragma omp parallel shared(x1) private(j)
+#pragma omp parallel shared(x1) private(j)
         {
-            #pragma omp critical (section1)
+#pragma omp critical (section1)
             {
                 y1 = 0;
                 for (j = 0; j < 3000; j++) {
@@ -291,9 +291,9 @@ void exercise3(){
 
         omp_set_num_threads(i);
         printf("\nNúmero de hilos: %d", i);
-        #pragma omp parallel shared(x2)
+#pragma omp parallel shared(x2)
         {
-            #pragma omp critical (section1)
+#pragma omp critical (section1)
             {
                 y2 = 0;
                 for (j = 0; j < 4000; j++) {
@@ -317,9 +317,9 @@ void exercise3(){
 
         omp_set_num_threads(i);
         printf("\nNúmero de hilos: %d", i);
-        #pragma omp parallel shared(x3)
+#pragma omp parallel shared(x3)
         {
-            #pragma omp critical (section1)
+#pragma omp critical (section1)
             {
                 y3 = 0;
                 for (j = 0; j < 5000; j++) {
@@ -340,7 +340,7 @@ void exercise3(){
     free(x3);
 }
 
-void exercise4(){
+void exercise4() {
     //For time measuring
     struct timeval start, end;
     double timeInvested;
@@ -351,7 +351,7 @@ void exercise4(){
     scanf("%d", &size);
     fflush(stdin);
 
-    int *array = (int*) malloc(sizeof(int) * size);
+    int *array = (int *) malloc(sizeof(int) * size);
 
     printf("Rellenando el array...\n");
     for (int i = 0; i < size; ++i) {
@@ -361,7 +361,7 @@ void exercise4(){
 
     gettimeofday(&start, NULL);
 
-    #pragma omp parallel for shared(array) lastprivate(max, min) firstprivate(max, min)
+#pragma omp parallel for shared(array) lastprivate(max, min) firstprivate(max, min)
     for (int i = 0; i < size; ++i) {
         if (array[i] > max) max = array[i];
         if (array[i] < min) min = array[i];
@@ -373,11 +373,9 @@ void exercise4(){
     printf("\tTiempo invertido: %f\t Máximo: %d\t Mínimo: %d", timeInvested, max, min);
 
 
-
-
 }
 
-void exercise5(){
+void exercise5() {
     //For time measuring
     struct timeval start, end;
     double timeInvested;
@@ -385,22 +383,22 @@ void exercise5(){
     double **x1, **x2, **x3;
     double *y1, *y2, *y3;
     double *z1, *z2, *z3;
-    x1 = (double**) malloc(sizeof(double) * 3000);
-    x2 = (double**) malloc(sizeof(double) * 4000);
-    x3 = (double**) malloc(sizeof(double) * 5000);
-    y1 = (double*) malloc(sizeof(double) * 3000);
-    y2 = (double*) malloc(sizeof(double) * 4000);
-    y3 = (double*) malloc(sizeof(double) * 5000);
-    z1 = (double*) malloc(sizeof(double) * 3000);
-    z2 = (double*) malloc(sizeof(double) * 4000);
-    z3 = (double*) malloc(sizeof(double) * 5000);
+    x1 = (double **) malloc(sizeof(double) * 3000);
+    x2 = (double **) malloc(sizeof(double) * 4000);
+    x3 = (double **) malloc(sizeof(double) * 5000);
+    y1 = (double *) malloc(sizeof(double) * 3000);
+    y2 = (double *) malloc(sizeof(double) * 4000);
+    y3 = (double *) malloc(sizeof(double) * 5000);
+    z1 = (double *) malloc(sizeof(double) * 3000);
+    z2 = (double *) malloc(sizeof(double) * 4000);
+    z3 = (double *) malloc(sizeof(double) * 5000);
 
     int j = 0;
 
     printf("Rellenando arrays...\n");
 
-    for(int i = 0; i < 3000; i++){
-        x1[i] = (double*) malloc(sizeof(double) * 3000);
+    for (int i = 0; i < 3000; i++) {
+        x1[i] = (double *) malloc(sizeof(double) * 3000);
         y1[i] = 3.0f;
         z1[i] = 0;
         for (j = 0; j < 3000; ++j) {
@@ -408,8 +406,8 @@ void exercise5(){
         }
     }
 
-    for(int i = 0; i < 4000; i++){
-        x2[i] = (double*) malloc(sizeof(double) * 4000);
+    for (int i = 0; i < 4000; i++) {
+        x2[i] = (double *) malloc(sizeof(double) * 4000);
         y2[i] = 3.0f;
         z2[i] = 0;
         for (j = 0; j < 4000; ++j) {
@@ -417,8 +415,8 @@ void exercise5(){
         }
     }
 
-    for(int i = 0; i < 5000; i++){
-        x3[i] = (double*) malloc(sizeof(double) * 5000);
+    for (int i = 0; i < 5000; i++) {
+        x3[i] = (double *) malloc(sizeof(double) * 5000);
         y3[i] = 3.0f;
         z3[i] = 0;
         for (j = 0; j < 5000; ++j) {
@@ -434,8 +432,8 @@ void exercise5(){
         printf("\nNúmero de hilos: %d", i);
 
         omp_set_num_threads(i);
-        #pragma omp parallel for collapse(2) shared(x1, y1)
-        for (j = 0;j < 3000;j++) {
+#pragma omp parallel for collapse(2) shared(x1, y1)
+        for (j = 0; j < 3000; j++) {
             for (int k = 0; k < 3000; ++k) {
                 z1[j] += x1[j][k] * y1[k];
             }
@@ -456,8 +454,8 @@ void exercise5(){
         omp_set_num_threads(i);
         printf("\nNúmero de hilos: %d", i);
 
-        #pragma omp parallel for collapse(2) shared(x2, y2)
-        for (j = 0;j < 4000;j++) {
+#pragma omp parallel for collapse(2) shared(x2, y2)
+        for (j = 0; j < 4000; j++) {
             for (int k = 0; k < 4000; ++k) {
                 z2[j] += x2[j][k] * y2[k];
             }
@@ -479,8 +477,8 @@ void exercise5(){
 
         printf("\nNúmero de hilos: %d", i);
 
-        #pragma omp parallel for collapse(2) shared(x3, y3)
-        for (j = 0;j < 5000;j++) {
+#pragma omp parallel for collapse(2) shared(x3, y3)
+        for (j = 0; j < 5000; j++) {
             for (int k = 0; k < 5000; ++k) {
                 z3[j] += x3[j][k] * y3[k];
             }
@@ -493,16 +491,15 @@ void exercise5(){
     }
 
 
-
-    for(int i = 0; i < 3000; i++){
+    for (int i = 0; i < 3000; i++) {
         free(x1[i]);
     }
 
-    for(int i = 0; i < 4000; i++){
+    for (int i = 0; i < 4000; i++) {
         free(x2[i]);
     }
 
-    for(int i = 0; i < 5000; i++){
+    for (int i = 0; i < 5000; i++) {
         free(x3[i]);
     }
 
@@ -517,8 +514,48 @@ void exercise5(){
     free(z3);
 }
 
+void threads_por_nivel(int nivel) {
+    printf("Nivel %d: Número de hilos en el nivel %d: "
+           "%d\n", nivel, nivel, omp_get_num_threads());
+}
 
-void exercise6(){
+void exercise6() {
+
+    printf("--------------------------\nEjecución con set nested\n--------------------------\n");
+    omp_set_nested(1);
+    omp_set_num_threads(2);
+    #pragma omp parallel
+    {
+        threads_por_nivel(1);
+        omp_set_num_threads(2);
+        #pragma omp parallel
+        {
+            threads_por_nivel(2);
+            omp_set_num_threads(2);
+            #pragma omp parallel
+            {
+                threads_por_nivel(3);
+            }
+        }
+    }
+
+    printf("\n--------------------------\nEjecución sin set nested\n--------------------------\n");
+    omp_set_nested(0);
+    omp_set_num_threads(2);
+    #pragma omp parallel
+    {
+        threads_por_nivel(1);
+        omp_set_num_threads(2);
+        #pragma omp parallel
+        {
+            threads_por_nivel(2);
+            omp_set_num_threads(2);
+            #pragma omp parallel
+            {
+                threads_por_nivel(3);
+            }
+        }
+    }
 
 }
 
