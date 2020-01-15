274be78d2c0ea48770d2829ca97081fd9a09f999
Alejandro Brugarolas <alejandro_brugarolas@hotmail.com>
2020-01-15 22:31:21 +0100

    aux

--- Practica20192020/solver.cu
+++ Practica20192020/solver.cu
@@ -11,30 +11,59 @@
 using namespace std;
 
 /**
-* Kernel del calculo de la solvation. Se debe anadir los parametros 
+* Kernel del calculo de la solvation. Se debe anadir los parametros
 */
 __global__ void escalculation (int atoms_r, int atoms_l, int nlig, float *rec_x_d, float *rec_y_d, float *rec_z_d, float *lig_x_d, float *lig_y_d, float *lig_z_d, float *ql_d,float *qr_d, float *energy_d, int nconformations){
-	
-	
+    int atomsLIdx = blockIdx.x * blockDim.x + threadIdx.x; // row
+    int atomsRIdx = blockIdx.y * blockDim.y + threadIdx.y; // col
+
+	float dist, total_elec = 0, miatomo[3], elecTerm;
+
+    int totalAtomLig = nconformations * nlig;
+
+
+	if (atomsRIdx < atoms_r && atomsLIdx < atoms_l){
+        for (int i = 0; i < totalAtomLig; i += nlig) {
+            miatomo[0] = *(lig_x_d + i + atomsLIdx);
+            miatomo[1] = *(lig_y_d + i + atomsLIdx);
+            miatomo[2] = *(lig_z_d + i + atomsLIdx);
+
+            //elecTerm = 0;
+            dist = calculaDistancia(rec_x_d[atomsRIdx], rec_y_d[atomsRIdx], rec_z_d[atomsRIdx], miatomo[0], miatomo[1], miatomo[2]);
+            energy_d[i / nlig] += (ql_d[atomsLIdx] * qr_d[atomsRIdx]) / dist;
+            if(energy_d[i / nlig] == -0.001414f){
+                printf("vaya qué bien");
+            }
+
+        }
+
+	}
+
 }
 
 
 /**
-* Funcion para manejar el lanzamiento de CUDA 
+* Funcion para manejar el lanzamiento de CUDA
 */
 void forces_GPU_AU (int atoms_r, int atoms_l, int nlig, float *rec_x, float *rec_y, float *rec_z, float *lig_x, float *lig_y, float *lig_z, float *ql ,float *qr, float *energy, int nconformations){
-	
-	cudaError_t cudaStatus; //variable para recoger estados de cuda
 
+	cudaError_t cudaStatus; //variable para recoger estados de cuda
 
 	//seleccionamos device
 	cudaSetDevice(0); //0 - Tesla K40 vs 1 - Tesla K230
 
 	//creamos memoria para los vectores para GPU _d (device)
 	float *rec_x_d, *rec_y_d, *rec_z_d, *qr_d, *lig_x_d, *lig_y_d, *lig_z_d, *ql_d, *energy_d;
-	int memSizeRec = sizeof(float) * atoms_r;
-	int memSizeLig = sizeof(float) * nlig * nconformations;
-	int memSizeQl = sizeof(float) * nlig;
+	int memSizeRec      = sizeof(float) * atoms_r;
+	int memSizeLig      = sizeof(float) * nlig * nconformations;
+	int memSizeQl       = sizeof(float) * nlig;
+    int memSizeEnergy   = sizeof(float) * nconformations;
+
+    cudaStatus = cudaMalloc((void**)&energy_d, memSizeEnergy);
+    if (cudaStatus != cudaSuccess){
+        fprintf(stderr, "Error al reservar memoria en la GPU para energy\n");
+        return;
+    }
 
     // ############ REC ############
     cudaStatus = cudaMalloc((void**)&rec_x_d, memSizeRec);
@@ -86,8 +115,12 @@ void forces_GPU_AU (int atoms_r, int atoms_l, int nlig, float *rec_x, float *rec
         return;
     }
 
-
-	//pasamos datos de host to device
+	//Pass data to the device
+	cudaStatus = cudaMemcpy(energy_d, energy, memSizeEnergy, cudaMemcpyHostToDevice);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "Error al transferir información HtD de energy\n");
+        return;
+    }
 
     // ############ REC ############
     cudaStatus = cudaMemcpy(qr_d, qr, memSizeRec, cudaMemcpyHostToDevice);
@@ -114,10 +147,7 @@ void forces_GPU_AU (int atoms_r, int atoms_l, int nlig, float *rec_x, float *rec
         return;
     }
 
-
     // ############ LIG ############
-
-
     cudaStatus = cudaMemcpy(ql_d, ql, memSizeQl, cudaMemcpyHostToDevice);
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "Error al transferir información HtD de ql\n");
@@ -141,29 +171,53 @@ void forces_GPU_AU (int atoms_r, int atoms_l, int nlig, float *rec_x, float *rec
         fprintf(stderr, "Error al transferir información HtD de lig_z\n");
         return;
     }
-	
-	//Definir numero de hilos y bloques
-	//printf("bloques: %d\n", (int)ceil(total_hilos/hilos_bloque)+1);
-	//printf("hilos por bloque: %d\n", hilos_bloque);
+
+	//Defines threads and blocks numbers
+	int xBlockSize = 16;
+    int yBlockSize = 1;
+    int threadsPerBlock = xBlockSize * yBlockSize;
+
+    dim3 block(ceil(atoms_l / xBlockSize), ceil(atoms_r / yBlockSize));
+    dim3 thread(xBlockSize, yBlockSize);
+
+	printf("Bloques x: %d\n", xBlockSize);
+	printf("Bloques y: %d\n", yBlockSize);
+	printf("Hilos por bloque: %d\n", threadsPerBlock);
 
 	//llamamos a kernel
-	//escalculation <<< block,thread>>> (atoms_r, atoms_l, nlig, rec_x_d, rec_y_d, rec_z_d, lig_x_d, lig_y_d, lig_z_d, ql_d, qr_d, energy_d, nconformations);
-	
+	escalculation <<< block,thread >>> (atoms_r, atoms_l, nlig, rec_x_d, rec_y_d, rec_z_d, lig_x_d, lig_y_d, lig_z_d, ql_d, qr_d, energy_d, nconformations);
+
 	//control de errores kernel
 	cudaDeviceSynchronize();
 	cudaStatus = cudaGetLastError();
-	if(cudaStatus != cudaSuccess) fprintf(stderr, "Error en el kernel %d\n", cudaStatus); 
+	if(cudaStatus != cudaSuccess) fprintf(stderr, "Error en el kernel %d\n", cudaStatus);
 
-	//Traemos info al host
+	//Gets the result back to the host
+    cudaStatus = cudaMemcpy(energy, energy_d, memSizeEnergy, cudaMemcpyDeviceToHost);
+    if (cudaStatus != cudaSuccess){
+        fprintf(stderr, "Error al transferir información DtH de energy\n");
+        return;
+    }
 
 	// para comprobar que la ultima conformacion tiene el mismo resultado que la primera
-	//printf("Termino electrostatico de conformacion %d es: %f\n", nconformations-1, energy[nconformations-1]); 
+	printf("Termino electrostatico de conformacion %d es: %f\n", nconformations-1, energy[nconformations-1]);
 
 	//resultado varia repecto a SECUENCIAL y CUDA en 0.000002 por falta de precision con float
 	//posible solucion utilizar double, probablemente bajara el rendimiento -> mas tiempo para calculo
 	printf("Termino electrostatico %f\n", energy[0]);
 
 	//Liberamos memoria reservada para GPU
+	cudaFree(rec_x_d);
+	cudaFree(rec_y_d);
+	cudaFree(rec_z_d);
+	cudaFree(lig_x_d);
+	cudaFree(lig_y_d);
+	cudaFree(lig_z_d);
+	cudaFree(ql_d);
+    cudaFree(qr_d);
+    cudaFree(energy_d);
+
+
 }
 
 /**
@@ -193,12 +247,12 @@ void forces_CPU_AU (int atoms_r, int atoms_l, int nlig, float *rec_x, float *rec
   int totalAtomLig = nconformations * nlig;
 
 	for (int k=0; k < totalAtomLig; k+=nlig){
-	  for(int i=0;i<atoms_l;i++){					
+	  for(int i=0;i<atoms_l;i++){
 			miatomo[0] = *(lig_x + k + i);
 			miatomo[1] = *(lig_y + k + i);
 			miatomo[2] = *(lig_z + k + i);
 
-			for(int j=0;j<atoms_r;j++){				
+			for(int j=0;j<atoms_r;j++){
 				elecTerm = 0;
         dist=calculaDistancia (rec_x[j], rec_y[j], rec_z[j], miatomo[0], miatomo[1], miatomo[2]);
 //				printf ("La distancia es %lf\n", dist);
@@ -207,7 +261,7 @@ void forces_CPU_AU (int atoms_r, int atoms_l, int nlig, float *rec_x, float *rec
 //        printf ("La carga es %lf\n", total_elec);
 			}
 		}
-		
+
 		energy[k/nlig] = total_elec;
 		total_elec = 0;
   }
@@ -218,12 +272,12 @@ void forces_CPU_AU (int atoms_r, int atoms_l, int nlig, float *rec_x, float *rec
 extern void solver_AU(int mode, int atoms_r, int atoms_l,  int nlig, float *rec_x, float *rec_y, float *rec_z, float *lig_x, float *lig_y, float *lig_z, float *ql, float *qr, float *energy_desolv, int nconformaciones) {
 
 	double elapsed_i, elapsed_o;
-	
+
 	switch (mode) {
 		case 0://Sequential execution
 			printf("\* CALCULO ELECTROSTATICO EN CPU *\n");
-			printf("**************************************\n");			
-			printf("Conformations: %d\t Mode: %d, CPU\n",nconformaciones,mode);			
+			printf("**************************************\n");
+			printf("Conformations: %d\t Mode: %d, CPU\n",nconformaciones,mode);
 			elapsed_i = wtime();
 			forces_CPU_AU (atoms_r,atoms_l,nlig,rec_x,rec_y,rec_z,lig_x,lig_y,lig_z,ql,qr,energy_desolv,nconformaciones);
 			elapsed_o = wtime() - elapsed_i;
@@ -231,9 +285,9 @@ extern void solver_AU(int mode, int atoms_r, int atoms_l,  int nlig, float *rec_
 			break;
 		case 1: //OpenMP execution
 			printf("\* CALCULO ELECTROSTATICO EN OPENMP *\n");
-			printf("**************************************\n");			
-			printf("**************************************\n");			
-			printf("Conformations: %d\t Mode: %d, CMP\n",nconformaciones,mode);			
+			printf("**************************************\n");
+			printf("**************************************\n");
+			printf("Conformations: %d\t Mode: %d, CMP\n",nconformaciones,mode);
 			elapsed_i = wtime();
 			forces_OMP_AU (atoms_r,atoms_l,nlig,rec_x,rec_y,rec_z,lig_x,lig_y,lig_z,ql,qr,energy_desolv,nconformaciones);
 			elapsed_o = wtime() - elapsed_i;
@@ -246,10 +300,10 @@ extern void solver_AU(int mode, int atoms_r, int atoms_l,  int nlig, float *rec_
 			elapsed_i = wtime();
 			forces_GPU_AU (atoms_r,atoms_l,nlig,rec_x,rec_y,rec_z,lig_x,lig_y,lig_z,ql,qr,energy_desolv,nconformaciones);
 			elapsed_o = wtime() - elapsed_i;
-			printf ("GPU Processing time: %f (seg)\n", elapsed_o);			
-			break; 	
+			printf ("GPU Processing time: %f (seg)\n", elapsed_o);
+			break;
 	  	default:
  	    	printf("Wrong mode type: %d.  Use -h for help.\n", mode);
-			exit (-1);	
-	} 		
+			exit (-1);
+	}
 }
